ba3c36981ec8c5d9408c65999c24784477525d89
Rajagopala Acharya <acharyarajagopala@gmail.com>
2024-09-14 15:03:15 +0530

    Slicing of strings early stage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main(){
-    
+fn first_word(string: &String) -> &str {
+    let bytes = string.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &string[0..i];
+        }
+    }
+
+    &string[..]
+}
+
+fn main() {
+    let s = String::from("My name is Raj");
+    let first_word = first_word(&s);
+    println!("{first_word}");
 }
